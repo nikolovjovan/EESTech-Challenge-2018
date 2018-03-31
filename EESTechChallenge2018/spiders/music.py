a05887fa8693b3081e4841176f3e6a0b0ffd53e7
Jovan Nikolov <nikolovjovan@hotmail.com>
2018-03-31 18:34:28 +0200

    Edit music spider, try to connect to the database

--- EESTechChallenge2018/spiders/music.py
+++ EESTechChallenge2018/spiders/music.py
@@ -1,33 +1,3 @@
-import scrapy
+import MySQLdb
 
-class Songs(scrapy.Item):
-	"""docstring for Songs"""
-	#def __init__(self, arg):
-		#super(Songs, self).__init__()
-	title = scrapy.Field()
-	file_urls = scrapy.Field()
-	files = scrapy.Field()
-
-class MusicSpider(scrapy.Spider):
-	name = "music"
-	allowed_domains = ["musicforprogramming.net"]
-	start_urls = ['http://musicforprogramming.net/']
-
-	count = 0
-
-	def parse(self, response):
-		# The name alone contains u'musicForProgramming("47: Abe Mangger");'
-		# So we strip out to get the name of the song only
-		title = response.css('title::text').extract_first()[len('musicForProgramming("'):-3]
-		src = response.xpath('//audio[@id="player"]/@src').extract_first()
-
-		self.count += 1
-		if self.count >= 46:
-			return
-
-		yield Songs(title=title, file_urls=[src])
-
-		next_url = response.xpath('//div[@id="episodes"]/a/@href').extract()[self.count]
-
-		if next_url is not None:
-			yield scrapy.Request(response.urljoin('http://musicforprogramming.net/' + next_url))
+db = MySQLdb.connect(host="eestech-challenge-2018.mysql.database.azure.com", user="jnikolov@eestech-challenge-2018", passwd="1234ABcd", port=3306, database="eestech")
